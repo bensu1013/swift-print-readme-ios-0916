6f381e4c99784217f7936562c610ed1f84b716a1
Ben Su <bensu1013@hotmail.com>
2016-08-29 15:35:32 -0400

    -a

---
+++ print.playground/Contents.swift
@@ -0,0 +1,9 @@
+var favoriteWord = "Pleibs"
+var favoriteColor = "Teal"
+var favoriteMusician = "Yankovic"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("Hello everybody! I heard all you \(favoriteWord) love the color \(favoriteColor).  I think \(favoriteMusician) likes it too!")
